39f1df489d1b04853b89adfb02d18a68a6ae1b37
Akash <akashparekh98@gmail.com>
2023-03-16 02:25:44 +0530

    Initial Views and URL's added for Challenges

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+def monthly_challenges(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "This Works!"
+    elif month == "februray":
+        challenge_text = "Walk Walk Wak"
+    elif month == "march":
+        challenge_text = "Complete this course by weekend"
+    else:
+        return HttpResponseNotFound("This month is not supported yet!")
+    return HttpResponse(challenge_text)
