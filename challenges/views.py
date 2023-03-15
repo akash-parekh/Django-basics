613340befbf64f73c8b7a6253c7dea194e6b153c
Akash <akashparekh98@gmail.com>
2023-03-16 03:46:17 +0530

    Added a list of month for '/challenges/'

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenge = {
     "january": "This Works!",
@@ -18,13 +19,26 @@ monthly_challenge = {
 
 # Create your views here.
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenge.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += "<li><a href =\"{0}\">{1}</a></li>".format(month_path, capitalized_month)
+
+    response_data = "<ul>{0}</ul>".format(list_items)
+    return HttpResponse(response_data)
+
 def monthly_challenges_by_number(request, month):
     months = list(monthly_challenge.keys())
 
     if month > len(months):
         return HttpResponseNotFound("Invalid month")
     redirect_month = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_url = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_url)
 
 def monthly_challenges(request, month):
     try:
