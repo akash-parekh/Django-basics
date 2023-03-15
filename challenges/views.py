bb4e3a0624ed358dc12159274764d12c8e3f3de3
Akash <akashparekh98@gmail.com>
2023-03-16 03:02:35 +0530

    Updated monthly_challenges and made it dynamic. Also added .gitignore file

--- challenges/views.py
+++ challenges/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+monthly_challenge = {
+    "january": "This Works!",
+    "febuary": "Walk Walk Walk",
+    "march": "Complete this course by weekend",
+    "april": "This Works!",
+    "may": "Walk Walk Walk",
+    "june": "Complete this course by weekend",
+    "july": "This Works!",
+    "august": "Walk Walk Walk",
+    "september": "Complete this course by weekend",
+    "october": "This Works!",
+    "november": "Walk Walk Walk",
+    "december": "Complete this course by weekend"
+}
 
 # Create your views here.
 
+def monthly_challenges_by_number(request, month):
+    months = list(monthly_challenge.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
 def monthly_challenges(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "This Works!"
-    elif month == "februray":
-        challenge_text = "Walk Walk Wak"
-    elif month == "march":
-        challenge_text = "Complete this course by weekend"
-    else:
-        return HttpResponseNotFound("This month is not supported yet!")
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenge[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("Invalid month")
